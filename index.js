6387060ffb05cabdfeb379cc06fcd264bda70700
unknown <salmanfursipro@gmail.com>
2023-05-23 17:31:20 +0600

    set inv user and pass

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxjq89v.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxjq89v.mongodb.net/?retryWrites=true&w=majority`;
+
 console.log(uri);
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -104,7 +109,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
